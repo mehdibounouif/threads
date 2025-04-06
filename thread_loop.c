0baaba408e433cb9849a9a2c7fdcb7b541bfe0cc
Mehdi Bounouif <mbounoui@c2r1p3.1337.ma>
2025-04-06 09:49:31 +0100

    some modify

--- thread_loop.c
+++ thread_loop.c
@@ -1,25 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   thread_loop.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbounoui <mbounoui@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/06 07:42:18 by mbounoui          #+#    #+#             */
+/*   Updated: 2025/04/06 09:05:11 by mbounoui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
 
 typedef struct	s_list
 {
-	int	value;
+	long	value;
 	pthread_mutex_t	mutex;
 }	t_list;
 
 void	*routine(void *args)
 {
 	t_list	*list;
-	int	i;
+	long	i;
 
 	list = (t_list *)args;
 	i = 0;
 	pthread_mutex_lock(&(list->mutex));
-	while (i++ < 100000)
+	while (i++ < 10000000000)
 		list->value++;
 	pthread_mutex_unlock(&(list->mutex));
+	return (NULL);
 }
+
+// Example of mutex:
+//
+// void	*routine(void *args)
+// {
+//		int	i = 0;
+//		int	lock = 0;
+//		while (i < 1000000)
+//		{
+//			if (lock == 1)
+//				wait until thread finished.
+//			lock = 1;
+//			i++;
+//			lock = 0;
+//		}
+//		return (NULL);
+// }
 
 int	main()
 {
@@ -31,6 +61,10 @@ int	main()
 	if (!(list = malloc(sizeof(t_list))))
 		return (1);
 	pthread_mutex_init(&(list->mutex), NULL);
+	
+	// pthread_create():
+	// 1 - Allocates resources for a new thread of execution within the current process 
+	// 2 - Create a new thread ID (stored in the pthread_t variable you provide)
 	while (i < 4)
 	{
 		if (pthread_create(&th[i], NULL, routine, list))
@@ -38,15 +72,25 @@ int	main()
 			printf("thread create failed!\n");
 			return (2);
 		}
+		printf("Thread %d is started with address %lu\n", i+1, th[i]);
+		i++;
+	}
+	// pthread_join():
+	// 1 - blocks the calling thread until the specified thread terminates.
+	// 2 - Retrieves the thread's return value (if any) and cleans up its resources.
+	i = 0;
+	while (i < 4)
+	{
 		if (pthread_join(th[i], NULL))
 		{
 			printf("thread failed execution!\n");
 			return (3);
 		}
-		i++;
+		printf("Thread %d is finished execution\n", i+1);
+		i++;	
 	}
 	pthread_mutex_destroy(&(list->mutex));
-	printf("Value = %d\n", list->value);
+	printf("Value = %ld\n", list->value);
 	return (0);
 
 }
