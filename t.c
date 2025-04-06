9edce44c2812ae479ea59a97291f351af6976d54
Mehdi Bounouif <mbounoui@c2r1p3.1337.ma>
2025-04-06 10:11:36 +0100

    exercise 1

---
+++ t.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t.c                                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbounoui <mbounoui@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/06 10:03:00 by mbounoui          #+#    #+#             */
+/*   Updated: 2025/04/06 10:10:38 by mbounoui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// the rece condition
+// the problem is uncall pthread_join() to block the calling thread until the specified thread terminated
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define NUM_THREADS 16
+
+void *thread(void *thread_id) {
+    int id = *((int *) thread_id);
+    printf("Hello from thread %d\n", id + 1);
+    return NULL;
+}
+
+int main() {
+	int	i;
+    pthread_t threads[NUM_THREADS];
+
+	i = 0;
+    while (i < NUM_THREADS) {
+        if (pthread_create(&threads[i], NULL, thread, &i))
+        {
+            fprintf(stderr, "pthread_create failed!\n");
+            return EXIT_FAILURE;
+        }
+		if (pthread_join(threads[i], NULL))
+		{
+            fprintf(stderr, " pthread_join failed!\n");
+            return EXIT_FAILURE;
+		}
+		i++;
+    }
+    return EXIT_SUCCESS;
+}
